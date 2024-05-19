4bf426424cc0728a483c791bf8205dd695b4e534
TimurKhazeev <timurhazeev69252@icloud.com>
2024-05-19 17:17:02 +0500

    Initial Commit

---
+++ BizBoard/BizBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BizBoardApp.swift
+//  BizBoard
+//
+//  Created by Тимур Хазеев on 19.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BizBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
